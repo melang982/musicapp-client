649609b19b5d1ce3e8dbe3bd6fa9a051cf55b76f
melang <melang4000@gmail.com>
2021-09-02 16:21:37 +0300

    search p.2

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,37 +1,60 @@
 import {useState} from 'react';
 import {useLazyQuery, gql} from '@apollo/client';
+import {NavLink} from 'react-router-dom';
 import Icon from './Icon';
 
 function Search() {
   const SEARCH_QUERY = gql `
     query SearchQuery($filter: String!) {
       artists(filter: $filter) {
-        id,
+        id
         name
+      }
+      albums(filter: $filter) {
+        id
+        title
+      }
+      tracks(filter: $filter) {
+        id
+        title
       }
     }
   `;
 
-  const [searchFilter, setSearchFilter] = useState('');
+  const [searchString, setSearchString] = useState(null);
   const [executeSearch, {
       data
     }
   ] = useLazyQuery(SEARCH_QUERY);
 
-  return <div>
-    Search
-    <input type="text" onChange={(e) => setSearchFilter(e.target.value)}/>
-    <button onClick={() => executeSearch({
+  function onSearch(str) {
+    if (str) {
+      executeSearch({
         variables: {
-          filter: searchFilter
+          filter: str
         }
       })
-}>OK</button>
-    {
-      data && data.artists.map((artist) => (<p key={artist.id}>
-        {artist.name}
-      </p>))
-    }
+    };
+
+    setSearchString(str);
+  };
+
+  return <div className="search">
+    <Icon icon='search'/>
+
+    <input type="text" placeholder="Type here to search" onInput={(e) => onSearch(e.target.value)}/>
+    <div className="search__results">
+      {
+        searchString && data && data.artists.map((artist) => <NavLink to={'/artist/' + artist.id} key={artist.id}>
+          {artist.name}
+        </NavLink>)
+      }
+      {
+        searchString && data && data.albums.map((album) => (<p key={album.id}>
+          {album.title}
+        </p>))
+      }
+    </div>
   </div>;
 
 }
